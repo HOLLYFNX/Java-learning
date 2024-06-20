d45afe1b933f8ad7fbb765b131f9c1e2db4dc6d4
Barbosa <mathisilva9@gmail.com>
2024-06-20 11:51:10 -0300

    primeiro commit java

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // 1 2 3 4 5 6 7 8 9 10
+        // para uma variável que inicia em 1, e vai até 10, mudando 1-por-1, faça:
+
+        for (int i = 1; i <= 10; i++){
+            for (int j = 1; j <= 10; j++){
+                System.out.println(j + "x" + i + "=" + j * i);
+                //j executa de 1 até 10, enquanto o i espera o j chegar a 10 para executar a repetição
+            }
+        }
+
+
+
+        }
+    }
